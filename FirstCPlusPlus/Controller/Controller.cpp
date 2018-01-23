ff70fbd28d169f4a532bbc07a48325ba0158da46
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-01-23 09:08:23 -0700

    Created a controller and runner.

---
+++ FirstCPlusPlus/Controller/Controller.cpp
@@ -0,0 +1,21 @@
+//
+//  Controller.cpp
+//  FirstCPlusPlus
+//
+//  Created by Johnsen, Matthew on 1/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+using namespace std;
+
+Controller :: Controller()
+{
+    cout << "I am in the constructor" << endl;
+}
+
+void Controller :: start()
+{
+    cout << "I am in the start method" << endl;
+}
---
+++ FirstCPlusPlus/Controller/Runner.cpp
@@ -0,0 +1,18 @@
+//
+//  Runner.cpp
+//  FirstCPlusPlus
+//
+//  Created by Johnsen, Matthew on 1/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include <iostream>
+#include "Controller.hpp"
+
+int main()
+{
+    Controller app;
+    app.start();
+    
+    return 0;
+}
