49f53aae5d331590f65d32a1b77c026f661c8718
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-01-23 08:49:45 -0700

    Created the first C++ project.

---
+++ FirstCPlusPlus/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  FirstCPlusPlus
+//
+//  Created by Johnsen, Matthew on 1/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // insert code here...
+    cout << "Hello, World!" << endl;
+    string word;
+    getline(cin, word);
+//    cin >> word;
+    cout << "Hello " << word << endl;
+    return 0;
+}
